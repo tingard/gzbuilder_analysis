small bugfix in reparametrization code. Allows rotation of classifications around image centre (needed for some weirdly created images)

gzbuilder_analysis/aggregation/__aggregation_result.py
@@ -8,6 +8,7 @@ from .__cluster import cluster_components
 from .__aggregate import aggregate_components
 
 
+# probably doesn't need to be a class, given it has no methods.
 class AggregationResult(object):
     def __init__(self, models, galaxy_data):
         self.clusters = cluster_components(
@@ -15,6 +16,7 @@ class AggregationResult(object):
             image_size=galaxy_data.shape,
             warn=False
         )
+        self.input_models = models
         self.aggregation_result = aggregate_components(self.clusters)
         self.phi = self.aggregation_result['disk']['roll']
         self.ba = self.aggregation_result['disk']['q']
@@ -34,8 +36,8 @@ class AggregationResult(object):
         else:
             self.spiral_pipeline = None
             self.spiral_arms = []
-        self.__model = aggregate_components(self.clusters)
-        self.__model['spiral'] = [
+        self.model = aggregate_components(self.clusters)
+        self.model['spiral'] = [
             (downsample(a.reprojected_log_spiral), DEFAULT_SPIRAL)
             for a in self.spiral_arms
         ]
@@ -49,24 +51,4 @@ class AggregationResult(object):
             self.errors.xs('c', level=1, drop_level=False),
         ))
         self.errors[unconstrained_errs.index] = np.inf
-        self.params = to_pandas(self.__model).rename('model')
-
-    def reset_params(self):
-        self.params = to_pandas(self.__model).rename('model')
-
-    def __calculate_spirals(self):
-        kw = dict(
-            centre=self.params[[('disk', 'mux'), ('disk', 'muy')]],
-            phi=self.params[('disk', 'roll')],
-            ba=self.params[('disk', 'q')]
-        )
-        [arm.modify_disk(**kw) for arm in self.spiral_arms]
-
-    # used during fitting
-    def update_params(self, new_params):
-        delta = (self.params - new_params).dropna()
-        self.params.update(new_params)
-        if np.any(delta.xs('disk', level=0) == 0):
-            self.__calculate_spirals()
-            return True
-        return False
+        self.params = to_pandas(self.model).rename('model')

gzbuilder_analysis/parsing/__init__.py
@@ -26,6 +26,7 @@ def parse_sersic_comp(comp, image_size, ignore_scale=False, **kwargs):
         'muy': image_size[0] - drawing['y'],
         'roll': np.deg2rad(roll),
         # in the original rendering code, minor axis was used instead of major
+        # and a scaling of 3 was present
         'Re': max(
             1e-5,
             minor_axis * (
@@ -38,7 +39,7 @@ def parse_sersic_comp(comp, image_size, ignore_scale=False, **kwargs):
             float(comp['value'][2]['value'])
             if comp['value'][2]['value'] is not None
             else 0.2
-        ) / (2 * 0.8), # correct for a factor of 2 and a 1/0.8 multiplier to standardise
+        ) / (2 * 0.8),  # correct for a factor of 2 and a 1/0.8 multiplier to standardise
         'c': 2,
         'n': 1,
     }
@@ -130,6 +131,35 @@ def scale_model(model, scale):
             for points, params in model['spiral']
         ]
     return model_out
+
+
+def rotate_model_about_centre(model, image_size, rotation):
+    crpos = np.array(image_size) / 2
+    rot_mx = np.array((
+        (np.cos(rotation), np.sin(rotation)),
+        (-np.sin(rotation), np.cos(rotation))
+    ))
+    new_model = {}
+    for comp in model:
+        if model[comp] is None:
+            new_model[comp] = None
+        elif comp == 'spiral':
+            new_spirals = [
+                ((np.dot(rot_mx, (points - crpos).T).T + crpos), deepcopy(params))
+                for points, params in model[comp]
+            ]
+            new_model[comp] = new_spirals
+        else:
+            new_model[comp] = deepcopy(model[comp])
+            p = np.array((new_model[comp]['mux'], new_model[comp]['muy']))
+            new_p = np.dot(rot_mx, p - crpos) + crpos
+            new_model[comp]['mux'] = new_p[0]
+            new_model[comp]['muy'] = new_p[1]
+            new_model[comp]['roll'] = (
+                new_model[comp]['roll'] + rotation
+            ) % np.pi
+            # Mod Pi not 2Pi due to rotational symmetry
+    return new_model
 
 
 def downsample(points, new_n=50):

gzbuilder_analysis/parsing/__reproject.py
@@ -1,8 +1,8 @@
 import numpy as np
 from copy import deepcopy
 
+
 def __rotation_matrix(r):
-#     r = np.deg2rad(d)
     return np.array((
         (np.cos(r), np.sin(r)),
         (-np.sin(r), np.cos(r))
@@ -18,33 +18,35 @@ def change_spiral_wcs(points, wcs_in, wcs_out):
 
 def reproject_sersic(comp, wcs_in, wcs_out):
     centre = np.array((comp['mux'], comp['muy']))
-    major_axis = (
-        centre
-        + np.dot(__rotation_matrix(comp['roll']), (comp['Re'], 0))
+    major_vec = np.dot(__rotation_matrix(-comp['roll']), (0, comp['Re']))
+    minor_vec = np.dot(__rotation_matrix(-comp['roll']), (comp['Re'] * comp['q'], 0))
+    major_axis = np.stack((centre, major_vec + centre), axis=1)
+    minor_axis = np.stack((centre, minor_vec + centre), axis=1)
+    new_centre = np.array(
+        wcs_out.all_world2pix(*wcs_in.all_pix2world(*centre, 0), 0)
     )
-    minor_axis = (
-        centre
-        + np.dot(
-            __rotation_matrix(comp['roll'] + np.pi / 2),
-            (comp['Re'] * comp['q'], 0)
-        )
+    new_major_axis = np.array(
+        wcs_out.all_world2pix(*wcs_in.all_pix2world(*major_axis, 0), 0)
     )
-    world_points = wcs_in.all_pix2world([
-        centre, major_axis, minor_axis
-    ], 0)
-    (
-        new_centre, new_major_axis, new_minor_axis
-    ) = wcs_out.all_world2pix(world_points, 0)
+    new_minor_axis = np.array(
+        wcs_out.all_world2pix(*wcs_in.all_pix2world(*minor_axis, 0), 0)
+    )
+    # world_points = wcs_in.all_pix2world([
+    #     centre, major_axis, minor_axis
+    # ], 0)
+    # (
+    #     new_centre, new_major_axis, new_minor_axis
+    # ) = wcs_out.all_world2pix(world_points, 0)
+    major_ax_vec = new_major_axis[:, 1] - new_centre
+    minor_ax_vec = new_minor_axis[:, 1] - new_centre
     comp_out = {
         **comp,
         'mux': new_centre[0],
         'muy': new_centre[1],
-        'roll': -np.arctan2(*(new_major_axis - new_centre)[::-1]),
-        'Re': np.sqrt(np.add.reduce((new_major_axis - new_centre)**2)),
+        'roll': -np.arctan2(major_ax_vec[0], major_ax_vec[1]),
+        'Re': np.linalg.norm(major_ax_vec)
     }
-    comp_out['q'] = np.sqrt(
-        np.add.reduce((new_minor_axis - new_centre)**2)
-    ) / comp_out['Re']
+    comp_out['q'] = np.linalg.norm(minor_ax_vec) / comp_out['Re']
     return comp_out
 
 

gzbuilder_analysis/rendering/jax/fit.py
@@ -6,6 +6,13 @@ from functools import reduce
 from .sersic import sersic, sersic_ltot, sersic_I
 from .spiral import vmap_polyline_distance
 
+EMPTY_SERSIC = pd.Series(
+    dict(mux=0, muy=0, Re=0.5, roll=0, q=1, I=0, n=1, c=2)
+)
+EMPTY_SERSIC_ERR = pd.Series(
+    dict(mux=1E-1, muy=1E-1, Re=0.1, roll=0.01, q=0.01, I=0.01, n=0.01, c=0.01)
+)
+
 
 def comp_bool_indexing(df):
     """Quickly convert a DataFrame to a dictionary, removing any NaNs
@@ -33,8 +40,15 @@ def to_reparametrization(agg_res, output_pandas=False):
     """Accept an aggregation result and reparametrize the result
     """
     disk = agg_res.params['disk']
-    bulge = agg_res.params['bulge']
-    bar = agg_res.params['bar']
+    if 'bulge' in agg_res.params:
+        bulge = agg_res.params['bulge']
+    else:
+        bulge = EMPTY_SERSIC
+    if 'bar' in agg_res.params:
+        bar = agg_res.params['bar']
+    else:
+        bar = EMPTY_SERSIC
+
     spirals = pd.DataFrame([
         agg_res.params[f'spiral{i}']
         for i in range(len(agg_res.spiral_arms))
@@ -82,12 +96,24 @@ def to_reparametrization(agg_res, output_pandas=False):
 
 def get_reparametrized_erros(agg_res):
     disk = agg_res.params['disk']
-    bulge = agg_res.params['bulge']
-    bar = agg_res.params['bar']
+    if 'bulge' in agg_res.params:
+        bulge = agg_res.params['bulge']
+    else:
+        bulge = EMPTY_SERSIC
+    if 'bar' in agg_res.params:
+        bar = agg_res.params['bar']
+    else:
+        bar = EMPTY_SERSIC
 
     disk_e = agg_res.errors['disk']
-    bulge_e = agg_res.errors['bulge']
-    bar_e = agg_res.errors['bar']
+    if 'bulge' in agg_res.errors:
+        bulge_e = agg_res.errors['bulge']
+    else:
+        bulge_e = EMPTY_SERSIC
+    if 'bar' in agg_res.errors:
+        bar_e = agg_res.errors['bar']
+    else:
+        bar_e = EMPTY_SERSIC
 
     errs = pd.DataFrame(
         [],
@@ -137,12 +163,11 @@ def get_limits(agg_res):
             'L': [0.0, np.inf],
             'mux': [-np.inf, np.inf],
             'muy': [-np.inf, np.inf],
-            'q': [0.3, 1.2],
+            'q': [0.4, 1.2],
             'roll': [-np.inf, np.inf],
             'Re': [0.01, np.inf],
         },
         'bulge': {
-            'c': [],
             'frac': [0.0, 0.99],
             'mux': [-np.inf, np.inf],
             'muy': [-np.inf, np.inf],
@@ -329,16 +354,14 @@ def _step(p, keys, n_spirals, base_model, model_err, psf, mask, target, sigma,
 
     # (4/6) calculate the model's NLL
     render_delta = (r - target) / sigma
-    masked_render_delta = ops.index_update(render_delta, mask, np.nan)
+    masked_render_delta = render_delta[~mask]
     model_nll = np.nansum(norm_nnlf(masked_render_delta.ravel()))
 
     # (5/6) calculate the parameter NLL (deviation from initial conditions)
     mu_p = np.array([base_model[k0][k1] for k0, k1 in keys])
     sigma_p = np.array([model_err[k0][k1] for k0, k1 in keys])
     param_delta = (p - mu_p) / sigma_p
-    masked_param_delta = ops.index_update(
-        param_delta, (sigma_p == np.inf), np.nan
-    )
+    masked_param_delta = param_delta[sigma_p != np.inf]
     param_nll = np.nansum(norm_nnlf(masked_param_delta.ravel()))
 
     # (6/6) return the sum of the NLLs