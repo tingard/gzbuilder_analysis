4b377f6b45d17cb6bc95e5ddad4fd1580e45bc65
Tim Lingard <tklingard@gmail.com>
2020-04-20 10:52:04 +0100

    adds easy way to read in aggregation / fitting results

--- gzbuilder_analysis/__init__.py
+++ gzbuilder_analysis/__init__.py
@@ -1,1 +1,37 @@
+import os
+import re
+import pandas as pd
+from tqdm import tqdm
 from .aggregation.__aggregation_result import AggregationResult
+
+
+def load_aggregation_results(path='output_files/aggregation_results'):
+    agg_results = pd.Series([], dtype=object)
+    with tqdm(
+        os.listdir(path),
+        desc='Loading aggregation results',
+        leave=False
+    ) as bar:
+        for f in bar:
+            if re.match(r'[0-9]+\.pkl.gz', f):
+                agg_results[int(f.split('.')[0])] = pd.read_pickle(
+                    os.path.join(path, f)
+                )
+    return agg_results
+
+
+def load_fit_results(path='output_files/tuning_results', include_bad=False):
+    fit_models = pd.Series([], dtype=object)
+    with tqdm(
+        os.listdir(path),
+        desc='Loading fitting results',
+        leave=False
+    ) as bar:
+        for f in bar:
+            if re.match(r'[0-9]+\.pickle.gz', f):
+                fit_result = pd.read_pickle(
+                    os.path.join(path, f)
+                )
+                if fit_result['res']['success'] or include_bad:
+                    fit_models[int(f.split('.')[0])] = fit_result
+    return fit_models.apply(pd.Series)
